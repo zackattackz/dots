164297d577c628a9e8d46b572cc56bc40e32d06a
Zachary Hanham <z@zmhanham.com>
2024-12-01 15:38:07 -0500

    lock service

--- pkgs/system/pre-install.sh
+++ pkgs/system/pre-install.sh
@@ -3,5 +3,5 @@
 PKG_TO_INSTALL="$PKG_TO_INSTALL dbus acpid turnstile seatd pipewire feh netcat void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree"
 DOTS="$DOTS out/home/dot-config/service/turnstile-ready/conf out/home/dot-local/bin/usv"
 SV_TO_ENABLE="$SV_TO_ENABLE dbus acpid turnstiled seatd"
-USR_SV_TO_ENABLE="$USR_SV_TO_ENABLE dbus pipewire wireplumber pipewire-pulse"
-USR_SV_DOWN="$USR_SV_DOWN pipewire"
+USR_SV_TO_ENABLE="$USR_SV_TO_ENABLE dbus pipewire wireplumber pipewire-pulse xautolock lockscreen"
+USR_SV_DOWN="$USR_SV_DOWN pipewire xautolock lockscreen"
