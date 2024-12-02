e773569e5f30bbd323f0b36e248bf8bcce528066
Zachary Hanham <z@zmhanham.com>
2024-12-02 14:55:27 -0500

    icon theme

--- pkgs/system/pre-install.sh
+++ pkgs/system/pre-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PKG_TO_INSTALL="$PKG_TO_INSTALL dbus acpid turnstile seatd pipewire feh netcat void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree"
-DOTS="$DOTS out/home/dot-config/service/turnstile-ready/conf out/home/dot-local/bin/usv out/system/etc/acpi/lid.sh out/system/etc/acpi/events/lid"
+DOTS="$DOTS out/home/dot-config/service/turnstile-ready/conf out/home/dot-local/bin/usv out/system/etc/acpi/lid.sh out/system/etc/acpi/events/lid out/system/usr/share/icons/default/index.theme"
 SV_TO_ENABLE="$SV_TO_ENABLE dbus acpid turnstiled seatd"
 USR_SV_TO_ENABLE="$USR_SV_TO_ENABLE dbus pipewire wireplumber pipewire-pulse xautolock lockscreen"
 USR_SV_DOWN="$USR_SV_DOWN pipewire xautolock lockscreen"
