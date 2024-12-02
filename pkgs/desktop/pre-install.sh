c92480df68570e9a8d387801852473d94f2155e5
Zachary Hanham <z@zmhanham.com>
2024-12-02 15:01:31 -0500

    add missing pkgs

--- pkgs/desktop/pre-install.sh
+++ pkgs/desktop/pre-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PKG_TO_INSTALL="$PKG_TO_INSTALL xorg-minimal xclip xorg-fonts xrandr xsetroot font-iosevka liberation-fonts-ttf mesa-dri vulkan-loader picom firefox tmux zip unzip hstr xset herbe xautolock slock alttab Fonts-TLWG font-sarasa-gothic xprop"
+PKG_TO_INSTALL="$PKG_TO_INSTALL xorg-minimal xclip xorg-fonts xrandr xsetroot font-iosevka liberation-fonts-ttf mesa-dri vulkan-loader picom firefox tmux zip unzip hstr xset herbe xautolock slock alttab Fonts-TLWG font-sarasa-gothic xprop xdotool xdg-utils xz"
 DOTS="$DOTS out/home/dot-xinitrc out/home/dot-config/picom/picom.conf out/home/dot-local/bin/xsetstatus out/home/dot-config/tmux/tmux.conf out/home/dot-config/tmux/theme.conf out/home/dot-local/bin/linkmenu.sh"
 
 [ -n "$CFG_INTEL" ] && PKG_TO_INSTALL="$PKG_TO_INSTALL intel-video-accel mesa-vulkan-intel mesa-32bit mesa-vulkan-intel-32bit vulkan-loader-32bit"
--- pkgs/devel/pre-install.sh
+++ pkgs/devel/pre-install.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-PKG_TO_INSTALL="$PKG_TO_INSTALL gcc libX11-devel libXft-devel libXinerama-devel make shellcheck tealdeer xtools python3-pip cmake gdb scdoc gd-devel meson"
+PKG_TO_INSTALL="$PKG_TO_INSTALL gcc libX11-devel libXft-devel libXinerama-devel make shellcheck tealdeer xtools python3-pip cmake gdb scdoc gd-devel meson pam-devel"
