b27cdc72c49e1fa6b12d15387cae492f61f916bd
Zachary Hanham <z@zmhanham.com>
2024-12-02 14:51:34 -0500

    dotdiff

---
+++ dotdiff.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+do_diffs() {
+    for f in $($3 find out/"$1" -type f); do
+        RELFILE=$(echo "$f" | sed 's|dot-|\.|g' | cut -d/ -f3-)
+        DIFFRES=$($3 diff "$f" "$2/$RELFILE")
+        [ $? = 1 ] && echo "$RELFILE" && echo "$DIFFRES"
+    done
+}
+
+make dots > /dev/null
+do_diffs home /home/z
+do_diffs system '' sudo
