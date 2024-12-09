a41af6d1c2e68d9dc3df481e92aa594ace0151e3
Zachary Hanham <z@zmhanham.com>
2024-12-09 16:44:04 -0500

    intel-ucode

--- pkgs/system/pre-install.sh
+++ pkgs/system/pre-install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 PKG_TO_INSTALL="$PKG_TO_INSTALL dbus acpid turnstile seatd pipewire feh netcat void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree systemd-boot"
+[ -n "$CFG_INTEL" ] && PKG_TO_INSTALL="$PKG_TO_INSTALL intel-ucode"
 DOTS="$DOTS out/home/dot-config/service/turnstile-ready/conf out/home/dot-local/bin/usv out/system/etc/acpi/lid.sh out/system/etc/acpi/events/lid out/system/usr/share/icons/default/index.theme out/system/etc/default/systemd-boot"
 SV_TO_ENABLE="$SV_TO_ENABLE dbus acpid turnstiled seatd"
 USR_SV_TO_ENABLE="$USR_SV_TO_ENABLE dbus pipewire wireplumber pipewire-pulse xautolock lockscreen"
