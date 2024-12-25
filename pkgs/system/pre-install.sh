a9449b7a7b0635fbfd44e301f710adbb15abdb5c
Zachary Hanham <z@zmhanham.com>
2024-12-25 18:24:05 -0500

    bluez

--- pkgs/system/pre-install.sh
+++ pkgs/system/pre-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PKG_TO_INSTALL="$PKG_TO_INSTALL dbus acpid turnstile seatd pipewire feh netcat void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree systemd-boot"
+PKG_TO_INSTALL="$PKG_TO_INSTALL dbus acpid turnstile seatd pipewire feh netcat void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree systemd-boot bluez libspa-bluetooth"
 [ -n "$CFG_INTEL" ] && PKG_TO_INSTALL="$PKG_TO_INSTALL intel-ucode"
 DOTS="$DOTS out/home/dot-config/service/turnstile-ready/conf out/home/dot-local/bin/usv out/system/etc/acpi/lid.sh out/system/etc/acpi/events/lid out/system/usr/share/icons/default/index.theme out/system/etc/default/systemd-boot"
 SV_TO_ENABLE="$SV_TO_ENABLE dbus acpid turnstiled seatd"
